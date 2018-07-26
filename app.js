24c462d5c288dcea03da7932eba065c7ee053a51
srikanth Karra <srikanthreddy376@gmail.com>
2018-07-26 19:48:10 +0200

    added index,new,partials

--- app.js
+++ app.js
@@ -6,43 +6,57 @@ var   express = require("express"),
       bodyParser = require("body-parser"),
       mongoose = require("mongoose");
 
-
-
  mongoose.connect("mongodb://localhost:27017/blog",{ useNewUrlParser: true });
  app.use(bodyParser.urlencoded({extended:true}));
+ app.use(express.static("public"));
  app.set("view engine","ejs");
 
 //schema of required pattern
  var blogSchenma = new mongoose.Schema({
   name:String,
   image:String,
-});
-// model of new blog
+  body:String,
+  created:{type: Date ,default: Date.now}
+
+ });
+// model congiguration
 var blog = mongoose.model("blog",blogSchenma);
 
+//RESTFULL ROUTES
+
 app.get("/",function(req,res){
-  res.send("Your app Worked");
+res.redirect("/blogs");
 });
 
-//post new post 
+app.get("/blogs",function(req,res){
+  blog.find({},function(err,blogs){
+   if(err){
+     console.log("Error");
+   }else{
+    res.render("index",{blogs :blogs});
+   }
+  });
+});
+
 app.post("/blogs",function(req,res){
   var name = req.body.name;
   var image = req.body.image;
+  var body = req.body.bodypost;
    var newBlog= {
     name:name,
     image:image,
-   
-  };
+    body :body,
+};
   blog.create(newBlog,function(err,newlyCreated){
     if(err){
         console.log(err);
     }else{
-      res.send("Scuccessfully updated in the blog");
+      res.redirect("/blogs");
     };
   }); 
 });
 //new Blog Route
-app.get("/blog/new",function(req,res){
+app.get("/blogs/new",function(req,res){
   res.render("new");
 });
 
